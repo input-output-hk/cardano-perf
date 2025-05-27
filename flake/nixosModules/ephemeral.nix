6441977ee3d8f80c5d7c3aee6339f7f73557340f
John Lotoski <john.lotoski@iohk.io>
2025-05-27 16:17:52 -0500

    module: add ephemeral

---
+++ flake/nixosModules/ephemeral.nix
@@ -0,0 +1,333 @@
+# nixosModule: profile-aws-ec2-ephemeral
+#
+# Originates from:
+# https://github.com/input-output-hk/cardano-parts/blob/main/flake/nixosModules/profile-aws-ec2-ephemeral.nix
+flake: {
+  flake.nixosModules.ephemeral = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: let
+    inherit (builtins) head;
+    inherit (lib) any getExe hasInfix hasPrefix mkForce mkIf mkOption removePrefix splitString;
+    inherit (lib.types) bool enum listOf str;
+    inherit (config.aws.instance) instance_type;
+
+    isDiskBackedStorageType = t: hasInfix "d" (head (splitString "." t));
+    isSpecialType = t: any (_: _) (map (e: hasPrefix e t) cfg.specialEphemeralInstanceTypePrefixes);
+
+    # NVMe containing ephemeral instance types generally need to have a "d" in
+    # the instance type, or belong to a special class.
+    hasEphemeral = isDiskBackedStorageType instance_type || isSpecialType instance_type;
+
+    cfg = config.services.aws.ec2.ephemeral;
+  in {
+    key = ./profile-aws-ec2-ephemeral.nix;
+
+    options.services.aws.ec2.ephemeral = {
+      enableMountOnCreation = mkOption {
+        type = bool;
+        default = true;
+        description = ''
+          Whether to ensure the ephemeral volume is mounted automatically after
+          it becomes available.
+        '';
+      };
+
+      enablePostMountService = mkOption {
+        type = bool;
+        default = true;
+        description = ''
+          Whether to enable post mounting service handling.
+        '';
+      };
+
+      fsOpts = mkOption {
+        type = listOf str;
+        default =
+          if cfg.fsType == "ext2"
+          then [
+            # An example file system performance tuning option for ext2
+            "noacl"
+            "noatime"
+            "nodiratime"
+          ]
+          else if cfg.fsType == "xfs"
+          then [
+            # An example file system performance tuning option for XFS
+            "logbsize=256k"
+          ]
+          else [];
+        description = ''
+          File system tuning options used during mounting.
+        '';
+      };
+
+      fsType = mkOption {
+        type = enum ["ext2" "xfs"];
+        default = "ext2";
+        description = ''
+          The file system to use for ephemeral storage.
+
+          The block devices will be formatted with this file system. If the
+          file system is to be changed, the instance will need to be
+          re-deployed and then stopped and restarted to re-instantiate the
+          ephemeral block device(s) at which point auto-format and relabelling
+          will occur.
+
+          NOTE: Changing to a file system other than ext2 or xfs will require
+          extending the nixos module code to support the new filesystem(s).
+        '';
+      };
+
+      label = mkOption {
+        type = str;
+        default = "ephemeral";
+        description = ''
+          The block device label to be applied to the ephemeral storage file
+          system.
+
+          Mounting by label allows the underlying device to be either a
+          direct NVMe block device or a RAID array.
+        '';
+      };
+
+      mountPoint = mkOption {
+        type = str;
+        default = "/ephemeral";
+        description = ''
+          The location to mount ephemeral storage.
+        '';
+      };
+
+      postMountScript = mkOption {
+        type = str;
+        default = ''
+          echo "No post mount script defined..."
+        '';
+        description = ''
+          Default post ephemeral block device mount script code, to be embedded
+          inside a bash writeShellApplication wrapper.
+        '';
+      };
+
+      postMountServiceName = mkOption {
+        type = str;
+        default = "ephemeral-post-mount";
+        description = ''
+          The systemd service name for post mount set up, excluding the
+          `.service` suffix.
+        '';
+      };
+
+      raidCfgFile = mkOption {
+        type = str;
+        default = "/etc/mdadm/mdadm.conf";
+        description = ''
+          The dynamically created and mutable raid configuration file for
+          ephemeral storage.
+        '';
+      };
+
+      raidDevice = mkOption {
+        type = str;
+        default = "/dev/md127";
+        description = ''
+          RAID often reverts to /dev/md127 due to auto scan and assemble
+          despite proper use of /etc/mdadm.conf, so to avoid device index
+          switching, default to use of the autoscan index from the time of
+          RAID array creation.
+        '';
+      };
+
+      serviceName = mkOption {
+        type = str;
+        default = "ephemeral-setup";
+        description = ''
+          The systemd service name for formatting ephemeral block devices,
+          excluding the `.service` suffix.
+        '';
+      };
+
+      specialEphemeralInstanceTypePrefixes = mkOption {
+        type = listOf str;
+        default = ["i3" "i7" "trn1"];
+        description = ''
+          Aws ec2 instance type prefixes which contain ephemeral NVMe block
+          devices without also containing the standard `d` identifier in the
+          instance type for disk backed storage.
+        '';
+      };
+
+      symlinkTarget = mkOption {
+        type = str;
+        default = "/dev/ephemeral";
+        description = ''
+          The symlink target linking to the ephemeral block storage file system.
+        '';
+      };
+    };
+
+    config = mkIf hasEphemeral {
+      fileSystems = {
+        "${cfg.label}" = {
+          inherit (cfg) fsType label mountPoint;
+
+          # The systemd service will handle the formatting logic.
+          autoFormat = false;
+
+          options =
+            [
+              # Wait until systemd ephemeral setup is complete so the file system will exist.
+              "x-systemd.requires=${cfg.serviceName}.service"
+              "x-systemd.after=${cfg.serviceName}.service"
+
+              # Required to avoid systemd order cycling problems that otherwise occur.
+              # The ephemeral file system will be auto-mounted on the first access attempt.
+              "x-systemd.automount"
+            ]
+            ++ cfg.fsOpts;
+        };
+      };
+
+      # Make /etc/mdadm.conf reflect the dynamically created mutable raid
+      # config file.
+      environment.etc."mdadm.conf".source = mkForce cfg.raidCfgFile;
+
+      systemd.services = {
+        ${cfg.serviceName} = {
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = getExe (pkgs.writeShellApplication {
+              name = cfg.serviceName;
+
+              runtimeInputs = with pkgs; [e2fsprogs fd jq kmod mdadm util-linux xfsprogs];
+              text = ''
+                set -euo pipefail
+
+                LABEL="${cfg.label}"
+                RAID_CFG="${cfg.raidCfgFile}"
+                RAID_DEV="${cfg.raidDevice}"
+                SYM_TGT="${cfg.symlinkTarget}"
+
+                IS_EMPTY() {
+                  DEV="$1"
+                  RESULT=$(lsblk --fs --json "$DEV" \
+                    | jq -r 'any(.blockdevices[]; (has("children") | not) and (.fstype == null))')
+
+                  if [ "$RESULT" = "true" ]; then
+                    return 0
+                  else
+                    return 1
+                  fi
+                }
+
+                mapfile -t INSTANCE_BD < <(fd 'nvme-Amazon_EC2_NVMe_Instance_Storage_AWS[0-9A-F]{17}$' /dev/disk/by-id/ | sort)
+                NUM_BD="''${#INSTANCE_BD[@]}"
+
+                echo "Number of ephemeral block devices detected are: $NUM_BD, comprised of:"
+                printf "%s\n" "''${INSTANCE_BD[@]}"
+
+                # Check ephemeral storage is available
+                if [ "$NUM_BD" -eq "0" ]; then
+                  echo "No ephemeral block devices found, aborting."
+                  exit 0
+                fi
+
+                # Check if all ephemeral storage is empty
+                EMPTY="true"
+                for DEV in "''${INSTANCE_BD[@]}"; do
+                  if ! IS_EMPTY "$DEV"; then
+                    echo "Device $DEV has a file system or holds partitions."
+                    EMPTY="false"
+                  fi
+                done
+
+                if [ "$EMPTY" = "false" ]; then
+                  echo "One or more ephemeral block devices are not empty; symlinking only..."
+                else
+                  echo "Ephemeral block device(s) appear to be empty, formatting and symlinking..."
+                fi
+
+                # Create the filesystem and symlink as applicable
+                if [ "$NUM_BD" -eq "1" ]; then
+                  if [ "$EMPTY" = "true" ]; then
+                    set -x
+                    mkfs -t ${cfg.fsType} -L "$LABEL" "''${INSTANCE_BD[@]}"
+                  fi
+                  ln -svf "''${INSTANCE_BD[@]}" "$SYM_TGT"
+                  set +x
+                elif [ "$NUM_BD" -gt "1" ]; then
+                  echo "Multiple ephemeral block devices are available..."
+                  if [ "$EMPTY" = "true" ]; then
+                    set -x
+                    mdadm --create "$RAID_DEV" --raid-devices="$NUM_BD" --level=0 "''${INSTANCE_BD[@]}"
+                    mdadm --detail --scan | tee "$RAID_CFG"
+                    mkfs -t ${cfg.fsType} -L "$LABEL" "$RAID_DEV"
+                    ln -svf "$RAID_DEV" "$SYM_TGT"
+                    set +x
+                  else
+                    if [ -b "$RAID_DEV" ]; then
+                      ln -svf "$RAID_DEV" "$SYM_TGT"
+                    fi
+                  fi
+                  exit 0
+                else
+                  echo "The number of ephemeral block devices was not properly recognized: $NUM_BD"
+                  exit 1
+                fi
+              '';
+            });
+          };
+        };
+
+        ${cfg.postMountServiceName} = {
+          after = ["${cfg.label}.mount"];
+          requires = ["${cfg.label}.mount"];
+          wantedBy = ["${cfg.label}.mount"];
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = getExe (pkgs.writeShellApplication {
+              name = cfg.serviceName;
+              text = ''
+                set -euo pipefail
+
+                ${cfg.postMountScript}
+              '';
+            });
+          };
+        };
+
+        "${cfg.serviceName}-mount-on-creation" = mkIf cfg.enableMountOnCreation {
+          wantedBy = ["multi-user.target"];
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = getExe (pkgs.writeShellApplication {
+              name = "${cfg.serviceName}-mount-on-creation";
+              text = ''
+                set -euo pipefail
+
+                while true; do
+                  echo "Sleeping 10 seconds until mount on created attempt..."
+                  sleep 10
+                  # shellcheck disable=SC2010
+                  if ls -1 / | grep -q ${removePrefix "/" cfg.mountPoint}; then
+                    echo "Found: ${cfg.mountPoint}"
+
+                    # Trigger the systemd auto-mount service
+                    ls ${cfg.mountPoint}
+
+                    touch ${cfg.mountPoint}/.mounted
+
+                    break
+                  fi
+                done
+              '';
+            });
+          };
+        };
+      };
+    };
+  };
+}
